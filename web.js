9fe2ed8e438bd1a100ca1a2354dc5f34c17deb3e
aiktat <aiktat@gmail.com>
2013-07-05 21:17:29 +0800

    read from index.html - redundant version

--- web.js
+++ web.js
@@ -4,10 +4,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//app.get('/', function(request, response) {
+//    var file_content = fs.readFileSync('index.html', 'utf-8');
+//    console.log(file_content);
+//    response.send(file_content);
+//});
+
 app.get('/', function(request, response) {
-    var file_content = fs.readFileSync('index.html', 'utf-8');
-    console.log(file_content);
-    response.send(file_content);
+    var file_content = fs.readFileSync('index.html');
+    var buffer = new Buffer(file_content);
+    var string = buffer.toString('utf-8');
+    console.log(string);
+    response.send(string);
 });
  
 var port = process.env.PORT || 5000;
