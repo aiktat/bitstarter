ec98b2609abd280013e50f4f86b7120472cd4f5c
aiktat <aiktat@gmail.com>
2013-07-05 21:03:34 +0800

    read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+var fs = require('fs');
+var buffer = require('buffer');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+    var file_content = fs.readFileSync('index.html', 'utf-8');
+    console.log(file_content);
+    response.send(file_content);
 });
  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
